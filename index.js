d20e191e1b75a306a06318133fab9bdeef20789f
Austin <Austin.flynn1992@gmail.com>
2023-11-22 13:14:47 -0600

    phase-1-algorithms-has-target-sum

--- index.js
+++ index.js
@@ -1,17 +1,47 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for(let i = 0; i<array.length; i++){
+    for(let j=i+1; j < array.length; j++){
+      if(array[i]+array[j]===target){
+        return true
+      }
+    }
+  }
+  return false
 }
-
-/* 
-  Write the Big O time complexity of your function here
-*/
-
-/* 
-  Add your pseudocode here
-*/
-
+// Write your algorithm here
 /*
-  Add written explanation of your solution here
+1)Take in array of numbers and our target number
+2)let i and j itterate over the array by 1 
+3)add i and j together to see if it is the target
+4)return true or false based on the sum
+*/
+
+
+
+
+  //Write the Big O time complexity of your function here
+/*
+O(n)
+
+*/ 
+ // Add your pseudocode here
+/*
+hasTargetSum()takes in the array of numbers and target number:
+i begins at 0 on the array and iterates array.length by i++:
+j begins at i+1 and iterates array.length by j++:
+if array i + array j is the target:  
+return true
+everything else is false
+
+*/
+  //Add written explanation of your solution here
+  /*
+  our function takes in the array of numbers, and the target number
+  we are setting i's positon to 0 and moving it along the array by 1 each time
+  we are doing the same with j but it will be 1 spot ahead on the array then i
+  we then add both array i and j together to see if the sum is our target each time it iterates over the array
+  and if so we return the value of true, if it is not our target number we return false. 
+  
 */
 
 // You can run `node index.js` to view these console logs
